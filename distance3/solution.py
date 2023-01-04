6f000db4605932a664434853ee8bac51d031a5d2
clemens <clemens.hoerhan@outlook.com>
2023-01-04 17:22:48 +0100

    Distance 3 finish

--- distance3/solution.py
+++ distance3/solution.py
@@ -1,74 +1,94 @@
 #!/usr/bin/python3
+""" This module is for the distance learning 3 tasks. """
+
 from math import floor
+import subprocess
+from threading import Thread
+from threading import Semaphore
 
 
-def readFile(filename):
+def read_file(filename):
+    """ Reads the lines from input file and returns them in a list. """
     numbers = []
     f_obj = open(filename, "r")
     for line in f_obj.readlines():
-        numbers.append(int(line))
+        numbers.append(line)
     return numbers
 
 
 def calc(value):
-    returnvalue = exec("./calc.sh " + value)
+    """ Uses the calc.sh script to calculate the new line value. """
+    returnvalue = int(subprocess.check_output(["./calc.sh", value]))
     return returnvalue
 
 
-def slowsort_start(arr, i, j):
-    for k in range(0, len(arr)):
-        arr[k] = calc(arr[k])
-    slowsort(arr, i, j)
+def slowsort_start(arr, i, j, semaphore):
+    """ Doubles the line numbers and sorts it afterwards with slowsort. """
+    with semaphore:
+        for idx, number in enumerate(arr):
+            arr[idx] = calc(number)
+        slowsort(arr, i, j)
 
 
-def slowsort(A, i, j):
+def slowsort(arr, i, j):
+    """ Sorts the list using the slowsort algorithm. """
     if i >= j:
         return
-    m = floor((i+j)/2)
-    slowsort(A, i, m)
-    slowsort(A, m+1, j)
+    mid = floor((i+j)/2)
+    slowsort(arr, i, mid)
+    slowsort(arr, mid+1, j)
 
-    if A[j] < A[m]:
-        h = A[j]
-        A[j] = A[m]
-        A[m] = h
+    if arr[j] < arr[mid]:
+        temp = arr[j]
+        arr[j] = arr[mid]
+        arr[mid] = temp
 
-    slowsort(A, i, j-1)
+    slowsort(arr, i, j-1)
 
 
-def writeNumbers(filename, numbers):
+def write_numbers(filename, numbers):
+    """ Writes the numbers into the specified file and sets the permissions. """
     f_obj = open(filename, "w")
-    s = ""
+    content = ""
     for number in numbers:
-        s += str(number) + "\n"
-    f_obj.write(s[0:-1])
+        content += str(number) + "\n"
+    f_obj.write(content[0:-1])
+    subprocess.call(["chmod", "600", filename])
 
 
-num1 = readFile("1-100.csv")
-num2 = readFile("101-200.csv")
-num3 = readFile("201-300.csv")
-num4 = readFile("301-400.csv")
-num5 = readFile("401-500.csv")
-num6 = readFile("501-600.csv")
+NUM1 = read_file("1-100.csv")
+NUM2 = read_file("101-200.csv")
+NUM3 = read_file("201-300.csv")
+NUM4 = read_file("301-400.csv")
+NUM5 = read_file("401-500.csv")
+NUM6 = read_file("501-600.csv")
 
-for i in range(0, 100):
-    num1[i] = calc(num1[i])
-    num2[i] = calc(num2[i])
-    num3[i] = calc(num3[i])
-    num4[i] = calc(num4[i])
-    num5[i] = calc(num5[i])
-    num6[i] = calc(num6[i])
+SEMAPHORE = Semaphore(2)
 
-slowsort_start(num1, 0, 99)
-slowsort_start(num2, 0, 99)
-slowsort_start(num3, 0, 99)
-slowsort_start(num4, 0, 99)
-slowsort_start(num5, 0, 99)
-slowsort_start(num6, 0, 99)
+THREAD1 = Thread(target=slowsort_start, args=(NUM1, 0, 99, SEMAPHORE))
+THREAD2 = Thread(target=slowsort_start, args=(NUM2, 0, 99, SEMAPHORE))
+THREAD3 = Thread(target=slowsort_start, args=(NUM3, 0, 99, SEMAPHORE))
+THREAD4 = Thread(target=slowsort_start, args=(NUM4, 0, 99, SEMAPHORE))
+THREAD5 = Thread(target=slowsort_start, args=(NUM5, 0, 99, SEMAPHORE))
+THREAD6 = Thread(target=slowsort_start, args=(NUM6, 0, 99, SEMAPHORE))
 
-writeNumbers("21-2100.csv", num1)
-writeNumbers("2101-2200.csv", num2)
-writeNumbers("2201-2300.csv", num3)
-writeNumbers("2301-2400.csv", num4)
-writeNumbers("2401-2500.csv", num5)
-writeNumbers("2501-2600.csv", num6)
+THREAD1.start()
+THREAD2.start()
+THREAD3.start()
+THREAD4.start()
+THREAD5.start()
+THREAD6.start()
+
+THREAD1.join()
+THREAD2.join()
+THREAD3.join()
+THREAD4.join()
+THREAD5.join()
+THREAD6.join()
+
+write_numbers("2-200.csv", NUM1)
+write_numbers("202-400.csv", NUM2)
+write_numbers("402-600.csv", NUM3)
+write_numbers("602-800.csv", NUM4)
+write_numbers("802-1000.csv", NUM5)
+write_numbers("1002-1200.csv", NUM6)
